993b2ba78392c97978055e89565dfdb5b8611a0c
lindaramawati <83651414+lindaramawati@users.noreply.github.com>
2022-09-28 22:51:14 +0700

    edit

--- application/views/Admin/index.php
+++ application/views/Admin/index.php
@@ -33,26 +33,12 @@
         <div class="header-area">
             <div class="header-left-sidebar">
                 <div class="logo">
-                    <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+                    <a href="index.php"><img src="assets/img/logo/logo.png" alt=""></a>
                 </div>
                 <div class="main-menu menu-hover">
                     <nav>
                         <ul>
-                            <li><a href="#">Home</a>
-                                <ul class="single-dropdown">
-                                    <li><a href="index.html">Fashion</a></li>
-                                    <li><a href="index-fashion-2.html">Fashion style 2</a></li>
-                                    <li><a href="index-fruits.html">fruits</a></li>
-                                    <li><a href="index-book.html">book</a></li>
-                                    <li><a href="index-electronics.html">electronics</a></li>
-                                    <li><a href="index-electronics-2.html">electronics style 2</a></li>
-                                    <li><a href="index-food.html">food & drink</a></li>
-                                    <li><a href="index-furniture.html">furniture</a></li>
-                                    <li><a href="index-handicraft.html">handicraft</a></li>
-                                    <li><a target="_blank" href="index-smart-watch.html">smart watch</a></li>
-                                    <li><a href="index-sports.html">sports</a></li>
-                                </ul>
-                            </li>
+                            <li><a href="#">Home</a></li>
                             <li><a href="#">Pages</a>
                                 <ul class="single-dropdown">
                                     <li><a href="about-us.html">about us</a></li>
@@ -65,179 +51,28 @@
                                     <li><a href="contact.html">contact</a></li>
                                 </ul>
                             </li>
-                            <li><a href="#">Latest <span>New</span></a>
-                                <div class="mega-menu-dropdown mega-dropdown-width">
-                                    <div class="mega-dropdown-style mega-common4 mb-40">
-                                        <h4 class="mega-subtitle"> Clothing</h4>
-                                        <ul>
-                                            <li><a href="#"> New Products</a></li>
-                                            <li><a href="#"> Jackets</a></li>
-                                            <li><a href="#"> Dress</a></li>
-                                            <li><a href="#"> Winter Collection</a></li>
-                                            <li><a href="#"> Ladis Jeans</a></li>
-                                            <li><a href="#"> Multipacks</a></li>
-                                            <li><a href="#"> Shorts</a></li>
-                                            <li><a href="#"> Night wear</a></li>
-                                            <li><a href="#"> Top Products</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="mega-dropdown-style mega-common4 mb-40">
-                                        <h4 class="mega-subtitle"> New Products</h4>
-                                        <ul>
-                                            <li><a href="#">View All</a></li>
-                                            <li><a href="#">New Trand</a></li>
-                                            <li><a href="#">Boots</a></li>
-                                            <li><a href="#">Flat Shoes</a></li>
-                                            <li><a href="#">Women Heels</a></li>
-                                            <li><a href="#">Slippers</a></li>
-                                            <li><a href="#">Socks & Tights</a></li>
-                                            <li><a href="#">Trainers</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="mega-dropdown-style mega-common4 mb-40">
-                                        <h4 class="mega-subtitle">Tranding</h4>
-                                        <ul>
-                                            <li><a href="#">Weeding</a></li>
-                                            <li><a href="#">Winter</a></li>
-                                            <li><a href="#">Holidays</a></li>
-                                            <li><a href="#">Night Party</a></li>
-                                            <li><a href="#">Outing Dress</a></li>
-                                            <li><a href="#">Outing Dress</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="mega-dropdown-style mega-common4 discount-mega-common4 mb-40">
-                                        <div class="mega-discount">
-                                            <h5>Make A Discount</h5>
-                                            <h2>UP TO 30%</h2>
-                                        </div>
-                                    </div>
-                                    <div class="mega-banner-img-2">
-                                        <a href="single-product.html"><img src="assets/img/bg/3.png" alt=""></a>
-                                    </div>
+                        </div>
+                        <div class="header-right-sidebar">
+                            <div class="header-search-cart-login">
+                                <div class="logo">
+                                    <a href="index.html">
+                                        <img src="assets/img/logo/logo.png" alt="">
+                                    </a>
                                 </div>
-                            </li>
-                            <li><a href="#">Shop</a>
-                                <div class="mega-menu-dropdown mega-dropdown-width-2">
-                                    <div class="mega-dropdown-style mega-common2 mega-common4">
-                                        <h4 class="mega-subtitle"> shop layout</h4>
-                                        <ul>
-                                            <li><a href="shop-grid-2-col.html"> grid 2 column</a></li>
-                                            <li><a href="shop-grid-3-col.html"> grid 3 column</a></li>
-                                            <li><a href="shop.html">grid 4 column</a></li>
-                                            <li><a href="shop-grid-box.html">grid box style</a></li>
-                                            <li><a href="shop-list-1-col.html"> list 1 column</a></li>
-                                            <li><a href="shop-list-2-col.html">list 2 column</a></li>
-                                            <li><a href="shop-list-box.html">list box style</a></li>
-                                            <li><a href="cart.html">shopping cart</a></li>
-                                            <li><a href="wishlist.html">wishlist</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="mega-dropdown-style mega-common2 mega-common4">
-                                        <h4 class="mega-subtitle">product details</h4>
-                                        <ul>
-                                            <li><a href="product-details.html">tab style 1</a></li>
-                                            <li><a href="product-details-2.html">tab style 2</a></li>
-                                            <li><a href="product-details-3.html"> tab style 3</a></li>
-                                            <li><a href="product-details-4.html">sticky style</a></li>
-                                            <li><a href="product-details-5.html">sticky style 2</a></li>
-                                            <li><a href="product-details-6.html">gallery style</a></li>
-                                            <li><a href="product-details-7.html">gallery style 2</a></li>
-                                            <li><a href="product-details-8.html">fixed image style</a></li>
-                                            <li><a href="product-details-9.html">fixed image style 2</a></li>
-                                        </ul>
-                                    </div>
+                                <div class="header-search">
+                                    <form action="#">
+                                        <input placeholder="Search What you want" type="text">
+                                        <button>
+                                            <i class="ti-search"></i>
+                                        </button>
+                                    </form>
                                 </div>
-                            </li>
-                            <li><a href="#">Kids</a>
-
-                            </li>
-                            <li><a href="#">Discount</a></li>
-                            <li><a href="#">Health <span>New</span></a></li>
-                            <li><a href="contact.html">contact</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                <div class="currency">
-                    <ul>
-                        <li><a href="#">EN</a></li>
-                        <li><a href="#">RN</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="header-right-sidebar">
-                <div class="header-search-cart-login">
-                    <div class="logo">
-                        <a href="index.html">
-                            <img src="assets/img/logo/logo.png" alt="">
-                        </a>
-                    </div>
-                    <div class="header-search">
-                        <form action="#">
-                            <input placeholder="Search What you want" type="text">
-                            <button>
-                                <i class="ti-search"></i>
-                            </button>
-                        </form>
-                    </div>
-                    <div class="header-login">
-                        <ul>
-                            <li><a href="login.php">Login</a></li>
-                            <li><a href="register.html">Reg</a></li>
-                        </ul>
-                    </div>
-                    <div class="header-cart cart-res">
-                        <a class="icon-cart" href="#">
-                            <i class="ti-shopping-cart"></i>
-                            <span class="shop-count pink">02</span>
-                        </a>
-                        <ul class="cart-dropdown">
-                            <li class="single-product-cart">
-                                <div class="cart-img">
-                                    <a href="#"><img src="assets/img/cart/1.jpg" alt=""></a>
+                                <div class="header-login">
+                                    <ul>
+                                        <li><a href="login.php">Login</a></li>
+                                        <li><a href="register.html">Reg</a></li>
+                                    </ul>
                                 </div>
-                                <div class="cart-title">
-                                    <h5><a href="#"> Bits Headphone</a></h5>
-                                    <h6><a href="#">Black</a></h6>
-                                    <span>$80.00 x 1</span>
-                                </div>
-                                <div class="cart-delete">
-                                    <a href="#"><i class="ti-trash"></i></a>
-                                </div>
-                            </li>
-                            <li class="single-product-cart">
-                                <div class="cart-img">
-                                    <a href="#"><img src="assets/img/cart/2.jpg" alt=""></a>
-                                </div>
-                                <div class="cart-title">
-                                    <h5><a href="#"> Bits Headphone</a></h5>
-                                    <h6><a href="#">Black</a></h6>
-                                    <span>$80.00 x 1</span>
-                                </div>
-                                <div class="cart-delete">
-                                    <a href="#"><i class="ti-trash"></i></a>
-                                </div>
-                            </li>
-                            <li class="single-product-cart">
-                                <div class="cart-img">
-                                    <a href="#"><img src="assets/img/cart/3.jpg" alt=""></a>
-                                </div>
-                                <div class="cart-title">
-                                    <h5><a href="#"> Bits Headphone</a></h5>
-                                    <h6><a href="#">Black</a></h6>
-                                    <span>$80.00 x 1</span>
-                                </div>
-                                <div class="cart-delete">
-                                    <a href="#"><i class="ti-trash"></i></a>
-                                </div>
-                            </li>
-                            <li class="cart-space">
-                                <div class="cart-sub">
-                                    <h4>Subtotal</h4>
-                                </div>
-                                <div class="cart-price">
-                                    <h4>$240.00</h4>
-                                </div>
-                            </li>
                             <li class="cart-btn-wrapper">
                                 <a class="cart-btn btn-hover" href="#">view cart</a>
                                 <a class="cart-btn btn-hover" href="#">checkout</a>
